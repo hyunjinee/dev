0bc5ed0cefed5f7b99cf8f9d67398e920b59f37c
hyunjin <leehj0110@kakao.com>
2021-02-15 23:50:35 +0900

    todolist  done

---
+++ React/todolist/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+import { TodoProvider } from "./TodoContext";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+function App() {
+  return (
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead></TodoHead>
+        <TodoList></TodoList>
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
+  );
+}
+
+export default App;
