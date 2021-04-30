ecd41cf81e214002edc50b2a1bb1c99429c9d6bd
hyunjin <leehj0110@kakao.com>
2021-04-30 13:00:19 +0900

    ✨ shopping cart with typescript & react

--- React/shoppingcart/src/App.tsx
+++ React/shoppingcart/src/App.tsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// Styles
+import { Wrapper, StyledButton } from './App.styles'; 
+
 // Types
 export type CartItemType = {
   id: number;
@@ -52,8 +63,31 @@ function App() {
       }, [] as CartItemType[])
       )
   }
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong....</div>
   return (
-    
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={()=> setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />  
+        </Badge> 
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
--- React/shoppingcart/src/index.tsx
+++ React/shoppingcart/src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient();
 
 
 ReactDOM.render(
+  <QueryClientProvider client={client}>
     <App />
+  </QueryClientProvider>
+    
   ,
   document.getElementById('root')
 );
