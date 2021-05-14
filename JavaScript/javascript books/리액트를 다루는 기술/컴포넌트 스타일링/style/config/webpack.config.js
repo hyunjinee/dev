3561de46b33a98a484a8ad9d93a30468631a9d00
hyunjin <leehj0110@kakao.com>
2021-05-14 19:38:36 +0900

    webpack config -> sass config change

--- JavaScript/javascript books/리액트를 다루는 기술/컴포넌트 스타일링/style/config/webpack.config.js
+++ JavaScript/javascript books/리액트를 다루는 기술/컴포넌트 스타일링/style/config/webpack.config.js
@@ -516,6 +516,7 @@ module.exports = function (webpackEnv) {
                     includePaths: [paths.appSrc + '/styles']
                   },
                   sourceMap: isEnvProduction && shouldUseSourceMap,
+                  prependData: `@import 'utils';`
                 }
               }),
               // Don't consider CSS imports dead code even if the
