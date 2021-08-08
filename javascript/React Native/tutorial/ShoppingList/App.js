213020396839a6885d45adbbd0f10105f24fe2a9
hyunjin <leehj0110@kakao.com>
2021-08-08 19:10:55 +0900

    first experience of react-native

---
+++ javascript/React Native/tutorial/ShoppingList/App.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, Alert } from 'react-native'
+import { uuid } from 'uuidv4'
+import Header from './components/Header'
+import AddItem from './components/AddItem'
+import ListItem from './components/ListItem';
+
+const App = () => {
+    const [items, setItems] = useState([
+        {
+            id: uuid(),
+            text: 'Milk',
+        },
+        {
+            id: uuid(),
+            text: 'Eggs',
+        },
+        {
+            id: uuid(),
+            text: 'Bread'
+        },
+        {
+            id: uuid(),
+            text: 'Juice'
+        }
+    ])
+    // Flag true if user is currently editing an item
+    const [editStatus, editStatusChange] = useState(false)
+
+    // State to capture info about the item being edited
+    const [editItemDetail, editItemDetailChange] = useState({
+        id: null,
+        text: null,
+    })
+
+    const [checkedItems, checkedItemChange] = useState([])
+
+    const deleteItem = id => {
+        setItems(prevItems => {
+            return prevItems.filter(item => item.id !== id)
+        })
+    }
+
+    const addItem = text => {
+        if (!text) {
+            Alert.Alert(
+                'No item entered',
+                'Please enter an item when adding to your shopping list',
+                [
+                    {
+                        text: 'Understood',
+                        style: 'cancel',
+                    },
+                ],
+                { cancelable: true }
+            )
+        } else {
+            setItems(prevItems => {
+                return [{ id: uuid(), text }, ...prevItems]
+            })
+        }
+    }
+
+
+    // Submit the users edits to the overall items state
+    const saveEditItem = (id, text) => {
+        setItems(prevItems => {
+            return prevItems.map(item =>
+                item.id === editItemDetail.id ? { id, text: editItemDetail.text } : item,
+            );
+        });
+        // Flip edit status back to false
+        editStatusChange(!editStatus);
+    };
+
+    // Event handler to capture users text input as they edit an item
+    const handleEditChange = text => {
+        editItemDetailChange({ id: editItemDetail.id, text });
+    };
+    // capture old items ID and text when user clicks edit
+    const editItem = (id, text) => {
+        editItemDetailChange({
+            id,
+            text,
+        });
+        return editStatusChange(!editStatus);
+    }
+
+    const itemChecked = (id, text) => {
+        const isChecked = checkedItems.filter(checkedItem => checkedItem.id === id);
+        isChecked.length
+          ? // remove item from checked items state (uncheck)
+            checkedItemChange(prevItems => {
+              return [...prevItems.filter(item => item.id !== id)];
+            })
+          : // Add item to checked items state
+            checkedItemChange(prevItems => {
+              return [...prevItems.filter(item => item.id !== id), {id, text}];
+            });
+      };
+    
+
+      return (
+        <View style={styles.container}>
+          <Header title="Shopping List" />
+          <AddItem addItem={addItem} />
+          <FlatList
+            data={items}
+            renderItem={({item}) => (
+              <ListItem
+                item={item}
+                deleteItem={deleteItem}
+                editItem={editItem}
+                isEditing={editStatus}
+                editItemDetail={editItemDetail}
+                saveEditItem={saveEditItem}
+                handleEditChange={handleEditChange}
+                itemChecked={itemChecked}
+                checkedItems={checkedItems}
+              />
+            )}
+          />
+        </View>
+      );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+
+
+export default App
