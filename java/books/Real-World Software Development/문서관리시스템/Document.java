a81bb189b7af9f882a9ef4b297003d0b6d93dd9e
hyunjinee <leehj0110@kakao.com>
2021-09-13 00:46:09 +0900

    4장

---
+++ java/books/Real-World Software Development/문서관리시스템/Document.java
@@ -0,0 +1,15 @@
+package com.hyunjin.chapter04;
+
+import java.util.Map;
+
+public class Document {
+    private final Map<String, String> attributes;
+
+    Document(final Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getAttribute(final String attributeName) {
+        return attributes.get(attributeName);
+    }
+}
